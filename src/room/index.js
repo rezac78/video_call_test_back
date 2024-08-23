8a35081e869e586102145171c412b2eca364eb4c
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-08-23 19:34:38 +0330

    git change 2

--- src/room/index.js
+++ src/room/index.js
@@ -1,13 +1,23 @@
 const { v4: uuidV4 } = require("uuid");
 
 const rooms = {};
+const callRequests = [];
 
 const roomHandler = (socket) => {
-  const createRoom = () => {
+  const createRoom = ({ name, phoneNumber }) => {
     const roomId = uuidV4();
     rooms[roomId] = [];
+    const newCallRequest = {
+      id: roomId,
+      name,
+      phoneNumber,
+      timestamp: Date.now(),
+    };
+    callRequests.push(newCallRequest);
     socket.emit("room-created", { roomId });
-    console.log("user created the room");
+
+    socket.broadcast.emit("new-call-request", newCallRequest);
+    console.log("user created the room", name, phoneNumber);
   };
   const joinRoom = ({ roomId, peerId }) => {
     if (rooms[roomId]) {
@@ -30,8 +40,21 @@ const roomHandler = (socket) => {
     rooms[roomId] = rooms[roomId].filter((id) => id !== peerId);
     socket.to(roomId).emit("user-disconnected", peerId);
   };
+  const deleteCall = (requestId) => {
+    const index = callRequests.findIndex((request) => request.id === requestId);
+    if (index !== -1) {
+      callRequests.splice(index, 1);
+      console.log(`Request with ID ${requestId} has been deleted.`);
+    }
+  };
+
+
   socket.on("create-room", createRoom);
   socket.on("join-room", joinRoom);
+  socket.on("get-call-requests", (callback) => {
+    callback(callRequests);
+  });
+  socket.on("delete-call-request", deleteCall);
 };
 
 module.exports = { roomHandler };
